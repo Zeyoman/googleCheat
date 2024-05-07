dfe2c25eab3b04b2a0e426db23ce315a09d04193
Zeyoman <alex10pump@gmail.com>
2024-05-07 21:50:03 +0200

    login fin

--- assets/app.js
+++ assets/app.js
@@ -8,27 +8,3 @@
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
-
-/**
- * Permet de faire le système de résultats
- */
-    const randomNumberPost = Math.floor(Math.random() * 1000000000) + 10000;
-    var randomNumberSeconds = Math.random()
-    randomNumberSeconds = randomNumberSeconds.toFixed(2);
-    document.getElementById('number-of-post').innerHTML ="Environ "+ randomNumberPost +" résultats ("+randomNumberSeconds+" secondes)"
-
-/**
- * Permet d'avoir des mots random
- */
-    fetch("https://trouve-mot.fr/api/categorie/22/5")
-        .then((response) => response.json())
-        .then((words) => {
-            console.log(words.length);
-            for(let i = 0; i < words.length; i++){
-                const word = words[i].name
-                const divWord = document.createElement('div')
-                divWord.classList.add('word')
-                divWord.innerHTML = word
-                document.getElementById('conseil-mots').appendChild(divWord)
-            }
-        });
